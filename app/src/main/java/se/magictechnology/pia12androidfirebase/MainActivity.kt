1954997ac8d78c7851c5ad931cf16dafd2738f8e
Bill Mårtensson <bill@magictechnology.se>
2024-02-26 13:46:19 +0100

    firebase

--- app/src/main/java/se/magictechnology/pia12androidfirebase/MainActivity.kt
+++ app/src/main/java/se/magictechnology/pia12androidfirebase/MainActivity.kt
@@ -1,6 +1,7 @@
 package se.magictechnology.pia12androidfirebase
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.getValue
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import se.magictechnology.pia12androidfirebase.ui.theme.Pia12androidfirebaseTheme
 
 // Skriva kod. Mera kod.
@@ -18,6 +24,8 @@ import se.magictechnology.pia12androidfirebase.ui.theme.Pia12androidfirebaseThem
 // Dev dev testa kod. Nu funkar det bra
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,6 +39,64 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        /*
+        val database = Firebase.database
+        val myRef = database.getReference("androidthings")
+        //myRef.push().setValue("Tjena")
+
+        val examplefruit = Fruit("Äpple", "Grön", 8)
+        val fruitRef = database.getReference("androidfruitbowl")
+        //fruitRef.push().setValue(examplefruit)
+
+        fruitRef.get().addOnSuccessListener {
+            //val getfruit = it.getValue<Fruit>()
+            //Log.i("PIA12DEBUG", getfruit!!.fruitname!!)
+            for(fruitsnap in it.children) {
+                val thefruit = fruitsnap.getValue<Fruit>()
+                Log.i("PIA12DEBUG", thefruit!!.fruitname!!)
+            }
+
+        }.addOnFailureListener {
+
+        }
+        */
+
+        /*
+        var auth: FirebaseAuth
+        auth = Firebase.auth
+
+        if(auth.currentUser == null) {
+            Log.i("PIA12DEBUG", "INTE INLOGGAD")
+        } else {
+            Log.i("PIA12DEBUG", "ÄR INLOGGAD")
+            Log.i("PIA12DEBUG", auth.currentUser!!.uid)
+        }
+        */
+
+        /*
+        auth.createUserWithEmailAndPassword("android@magictechnology.se", "hemligt").addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                Log.i("PIA12DEBUG", "REGISTRERING OK")
+            } else {
+                Log.i("PIA12DEBUG", "REGISTRERING FAIL")
+            }
+        }
+        */
+
+        //auth.signOut()
+
+        /*
+        auth.signInWithEmailAndPassword("android@magictechnology.se", "hemligt").addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                Log.i("PIA12DEBUG", "LOGIN OK")
+            } else {
+                Log.i("PIA12DEBUG", "LOGIN FAIL")
+            }
+        }
+         */
+
+
     }
 
     fun dologin() {
@@ -53,3 +119,10 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+
+data class Fruit(
+    val fruitname : String? = null,
+    val fruitcolor : String? = null,
+    val score : Int? = null
+)
