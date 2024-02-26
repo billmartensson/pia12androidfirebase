154e6ea468eba2c74e8b105868503b4513dee805
Bill Mårtensson <bill@magictechnology.se>
2024-02-26 09:47:13 +0100

    ny funktion funkar

--- app/src/main/java/se/magictechnology/pia12androidfirebase/MainActivity.kt
+++ app/src/main/java/se/magictechnology/pia12androidfirebase/MainActivity.kt
@@ -15,7 +15,7 @@ import se.magictechnology.pia12androidfirebase.ui.theme.Pia12androidfirebaseThem
 // Skriva kod. Mera kod.
 // Annan person
 
-// Dev dev testa kod
+// Dev dev testa kod. Nu funkar det bra
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
